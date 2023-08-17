3de3c1a58ca98a51192b3a38f34fd9f0577b7ce6
Soham Nandy <soham.nandy2006@gmail.com>
2023-08-18 00:20:07 +0530

    baal

--- app.py
+++ app.py
@@ -3,9 +3,15 @@ from flask import Flask,render_template
 app = Flask(__name__)
 
 
+goal_list = {"Plant Trees":[50000,29000,0,"images/donate1.jpg"],
+             "Animal Healthcare":[50000,25000,0,"images/donate2.jpg"],
+             "Water Drone Research":[50000,0,0,"images/donate3.jpg"],
+             "Awareness Campaigns":[50000,12313,0,"images/donate4.jpg"], 
+             }
+
 @app.route("/")
 def index():
-    return render_template("index.html")
+    return render_template("index.html",goal_list=goal_list)
 
 
 @app.route("/about/")
